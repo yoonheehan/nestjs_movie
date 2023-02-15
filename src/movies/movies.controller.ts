e06afe7f2adc96a07415f19e40e768a7eda38d1b
YoonHee Han <grayman1234@naver.com>
2023-02-16 08:25:44 +0900

    영화하는중

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,40 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
+
+@Controller('movies')
+export class MoviesController {
+
+    @Get()
+    getAll(){
+        return "This will return all movies";
+    }
+
+    @Get("search")
+    search(@Query('year') searchingYear) {
+        return `We are searching for a movie made after: ${searchingYear}`;
+    }
+
+    @Get("/:id")
+    getOne(@Param("id") movieId: string){
+        return `This will return one movie with the id: ${movieId}`;
+    }
+
+    @Post()
+    create(@Body() movieData){
+        return movieData;
+    }
+
+    @Delete("/:id")
+    remove(@Param('id') movieId: string){
+        return `This will delete a movie with the id: ${movieId}`;
+    }
+
+    @Patch('/:id')
+    patch(@Param('id') movieId: string, @Body() updateData){
+        return {
+            updatedMovie: movieId,
+            ...updateData,
+        };
+    }
+
+    
+}
